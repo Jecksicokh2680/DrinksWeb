72e8d87bad14d2299c8f7cfbe3ef360cfcb96d91
Jecksicokh2680 <alex_3r@hotmail.com>
2026-07-03 04:05:40 -0500

    traslados ok

--- Panel.php
+++ Panel.php
@@ -81,8 +81,8 @@ body{ margin:0; display:flex; min-height:100vh; font-family:Arial,sans-serif; }
                 <div class="accordion-body">
                     <a class="nav-link" href="Transfers.php" target="contentFrame">➕ Transferencia</a>
                     <a class="nav-link" href="Conteo.php" target="contentFrame">🧮 Conteo Web</a>
-                    <a class="nav-link" href="SolicitudAnulacion.php" target="contentFrame">🧮 Solicitud Anulación</a>
-                    <a class="nav-link" href="ListaFactDia.php" target="contentFrame">🧮 Facturas del Día</a>
+                    <a class="nav-link" href="SolicitudAnulacion.php" target="contentFrame">🧮 Lista Solicitud Anulación</a>
+                    <a class="nav-link" href="ListaFactDia.php" target="contentFrame">🧮 Listado Facturas del Día</a>
                     <a class="nav-link" href="Calculadora.php" target="contentFrame">📄 Calculadora</a>
                     <a class="nav-link" href="CierreDef.php" target="contentFrame">🧮 Cierre Diario Cajero </a>
                     <a class="nav-link" href="TrasladosMercancia.php" target="contentFrame">🧮 Traslados de mercancia</a>
