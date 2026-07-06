7c43debe174ad304f92058ea40e478194bcd4505
Jecksicokh2680 <alex_3r@hotmail.com>
2026-07-06 04:45:47 -0500

    panelx7

--- Panel.php
+++ Panel.php
@@ -234,7 +234,7 @@ body {
                             <div id="adminOp" class="accordion-collapse collapse">
                                 <div class="accordion-body">
                                     <a class="nav-link" href="ValorInventario.php" target="contentFrame">Dashboard BNMA</a>
-                                    <a class="nav-link" href="Compras.php" target="contentFrame">Compras BNMA</a>
+                                    <a class="nav-link" href="Compras.php" target="contentFrame">Ver Compras BNMA</a>
                                     <a class="nav-link" href="CierreCajerosTodos.php" target="contentFrame">Resumen de Cierres Bnma </a>
                                     <a class="nav-link" href="ValorInventariox.php" target="contentFrame">Dashboard Historico</a>    
                                     <a class="nav-link" href="DashBoard3.php" target="contentFrame">DashBoard Compras Vs Ventas</a>                                                                                                                
