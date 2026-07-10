dca04223ca151564dc3fe00f1c04b7e5622ae1e2
Jecksicokh2680 <alex_3r@hotmail.com>
2026-07-10 07:47:00 -0500

    audi_12

--- Panel.php
+++ Panel.php
@@ -196,7 +196,7 @@ body {
                         🧮 Ver Transferencias Bre-B desde el Banco
                         </a>
                         <a class="nav-link" href="LectorEmailFacturas.php" target="contentFrame">🧮 Ver Que llega Hoy</a>   
-                        <a class="nav-link" href="AuditoriaPedido.php" target="contentFrame"> 🧮 Auditoria Pedidos</a>                 
+                                 
                     </div>
 
                     <?php if ($EsJefeBodega): ?>
@@ -214,6 +214,7 @@ body {
                     </h6>
                     <div class="menu-seccion ps-2">
                         <a class="nav-link" href="Conteo.php" target="contentFrame">🧮 Grabar Conteo Web</a>
+                        <a class="nav-link" href="AuditoriaPedido.php" target="contentFrame"> 🧮 Auditoria Pedidos</a>        
                         <a class="nav-link" href="vertunel.php" target="contentFrame">🧮 Ver Tunel</a>                    
                         <a class="nav-link" href="#" onclick="window.open('Chat.php', 'ChatInterno', 'width=450,height=300,scrollbars=yes,resizable=No'); return false;">🧮 Chat Interno </a>
                     </div>
